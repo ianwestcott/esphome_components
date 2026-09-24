cab265024f3b8a8c010b65e74d2825865aa35dcd
Ian Westcott <iandwestcott@gmail.com>
2026-09-23 21:36:57 -0400

    Remove const qualifier from dump_summary method
    attempting to fix compilation error in ESPHome >2026.8: 
    
    ```
    src/esphome/components/seesaw/binary_sensor/../seesaw.h:115:15: error: 'std::string esphome::seesaw::SeesawGPIOPin::dump_summary() const' marked 'override', but does not override
    ```

--- components/seesaw/seesaw.h
+++ components/seesaw/seesaw.h
@@ -112,7 +112,7 @@ class SeesawGPIOPin : public GPIOPin {
   void pin_mode(gpio::Flags flags) override;
   bool digital_read() override;
   void digital_write(bool value) override;
-  std::string dump_summary() const override;
+  std::string dump_summary() const;
 
   void set_parent(Seesaw *parent) { parent_ = parent; }
   void set_pin(uint8_t pin) { pin_ = pin; }
